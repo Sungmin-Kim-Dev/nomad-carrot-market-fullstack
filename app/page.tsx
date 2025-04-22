c24e94611ee2c093aa39cb0fbadee0ae7bf62978
Sungmin_Kim <dreamerk24dev@gmail.com>
2025-04-23 00:40:20 +0900

    form modifiers

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 export default function Home() {
   return (
     <main className="flex h-screen items-center justify-center bg-gray-100 p-5 sm:bg-red-50 md:bg-green-100 lg:bg-cyan-200 xl:bg-orange-200 2xl:bg-purple-200">
-      <div className="flex w-full max-w-[640px] flex-col md:flex-row gap-2 rounded-3xl bg-white p-5 shadow-lg">
+      <div className="flex w-full max-w-[640px] flex-col gap-2 rounded-3xl bg-white p-5 shadow-lg md:flex-row">
         <input
-          className="h-12 w-full rounded-full bg-gray-200 pl-5 ring ring-transparent transition-shadow outline-none placeholder:drop-shadow focus:ring-orange-500 focus:ring-offset-2"
-          type="text"
-          placeholder="Search Here..."
+          className="h-12 w-full rounded-full bg-gray-200 pl-5 ring ring-transparent transition-shadow outline-none placeholder:drop-shadow focus:ring-green-500 focus:ring-offset-2 invalid:focus:ring-red-500 peer"
+          type="email"
+          required
+          placeholder="Email Address"
         />
-        <button className="rounded-full bg-black py-2 font-medium text-white transition-transform outline-none active:scale-90 md:px-10">
+        <span className="text-red-500 font-medium hidden peer-invalid:block">Email is required.</span>
+        <button className="rounded-full bg-zinc-800 py-2 font-medium text-white transition-transform outline-none peer-invalid:bg-red-400 active:scale-90 md:px-10">
           Search
         </button>
       </div>
