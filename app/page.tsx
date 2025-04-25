cdb25bdc37cee3920d8161970325c3eba9ecc79d
Sungmin_Kim <dreamerk24dev@gmail.com>
2025-04-25 22:52:50 +0900

    theme in css file

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,16 @@
 export default function Home() {
   return (
     <main className="flex h-screen items-center justify-center bg-gray-100 p-5 sm:bg-red-50 md:bg-green-100 lg:bg-cyan-200 xl:bg-orange-200 2xl:bg-purple-200">
-      <div className="flex w-full max-w-[640px] flex-col gap-3 rounded-3xl bg-white p-5 shadow-lg group">
+      <div className="group flex w-full max-w-[640px] flex-col gap-3 rounded-3xl bg-white p-5 shadow-lg">
         <input
           type="text"
           className="w-full bg-gray-100"
           placeholder="Write your Email"
         />
-        <span className="group-focus-within:block hidden">Make sure it is a valid email...</span>
-        <button>Submit</button>
+        <span className="hidden group-focus-within:block">
+          Make sure it is a valid email...
+        </span>
+        <button className="bg-mint-500">Submit</button>
       </div>
     </main>
   );
