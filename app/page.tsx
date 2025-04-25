cd3530cc89dad913fed7cd3489fdfc2c140ff20f
Sungmin_Kim <dreamerk24dev@gmail.com>
2025-04-25 19:01:11 +0900

    group 1

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,9 @@ export default function Home() {
       <div className="flex w-full max-w-[640px] flex-col gap-3 rounded-3xl bg-white p-5 shadow-lg">
         {["Jack", "Me", "Nico", "Perfect", "Liz", "Vicky", ""].map(
           (person, index) => (
-            <div key={index} className="flex items-center gap-5">
+            <div key={index} className="flex items-center gap-5 group">
               <div className="size-10 rounded-full bg-blue-400"></div>
-              <span className="text-lg font-medium empty:bg-gray-300 empty:w-24 empty:h-5 empty:rounded-full empty:animate-pulse">{person}</span>
+              <span className="text-lg font-medium empty:bg-gray-300 empty:w-24 empty:h-5 empty:rounded-full empty:animate-pulse group-hover:text-red-500">{person}</span>
               <div className="flex size-6 items-center justify-center rounded-full bg-red-500 text-white relative">
                 <span className="z-10">{index}</span>
                 <div className="size-6 rounded-full animate-ping bg-red-500 absolute"></div>
