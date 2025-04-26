1dc0804f0df04032bc443f9d23059b3ce0b2d6b0
Sungmin_Kim <dreamerk24dev@gmail.com>
2025-04-26 22:53:50 +0900

    form components

--- app/create-account/page.tsx
+++ app/create-account/page.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import { ChatBubbleOvalLeftEllipsisIcon } from "@heroicons/react/24/solid";
 import { ChatBubbleBottomCenterTextIcon } from "@heroicons/react/24/solid";
+import FormInput from "@/components/form-input";
+import FormButton from "@/components/form-btn";
 
 export default function CreateAccount() {
   return (
@@ -9,17 +11,22 @@ export default function CreateAccount() {
         <h1 className="text-2xl">Hello!</h1>
         <h2 className="text-xl">Fill in the form below to join us!</h2>
       </div>
-      <form action="" className="flex flex-col gap-3">
-        <div className="flex flex-col gap-2">
-          <input
-            className="h-10 w-full rounded-md border-none bg-transparent ps-2 ring-1 ring-neutral-200 placeholder:text-neutral-400 focus:ring-2 focus:ring-orange-500 focus:outline-none"
-            type="text"
-            placeholder="Username"
-            required
-          />
-          <span className="font-medium text-red-500">Input error</span>
-        </div>
-        <button className="primary-btn h-10">Create Account</button>
+      <form action="" className="flex flex-col gap-5">
+        <FormInput type="text" placeholder="Username" required errors={[]} />
+        <FormInput type="email" placeholder="Email" required errors={[]} />
+        <FormInput
+          type="password"
+          placeholder="Password"
+          required
+          errors={[]}
+        />
+        <FormInput
+          type="password"
+          placeholder="Confirm Password"
+          required
+          errors={[]}
+        />
+        <FormButton loading={false} text="Create Account" />
       </form>
       <div className="h-px w-full bg-neutral-500"></div>
       <div>
