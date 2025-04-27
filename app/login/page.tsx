9b7f6221935f8f1276e4d879dddf626fda5f11f2
Sungmin_Kim <dreamerk24dev@gmail.com>
2025-04-27 18:02:24 +0900

    route handlers(old way)

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,20 @@
+"use client";
+
 import FormInput from "@/components/form-input";
 import FormButton from "@/components/form-btn";
 import SocialLogin from "@/components/social-login";
 
 export default function LogIn() {
+  const onClick = async () => {
+    const response = await fetch("/api/users", {
+      method: "POST",
+      body: JSON.stringify({
+        username: "nico",
+        password: "1234",
+      }),
+    });
+    console.log(await response.json());
+  };
   return (
     <div className="flex flex-col gap-10 px-6 py-8">
       <div className="flex flex-col gap-2 *:font-medium">
@@ -17,8 +29,10 @@ export default function LogIn() {
           required
           errors={[]}
         />
-        <FormButton loading={false} text="Log In" />
       </form>
+      <span onClick={onClick}>
+        <FormButton loading={false} text="Log In" />
+      </span>
       <SocialLogin />
     </div>
   );
