1d405efdcc443cae2f2e69625f78a924919448f6
Sungmin_Kim <dreamerk24dev@gmail.com>
2025-04-29 00:24:33 +0900

    text commented

--- lib/db.ts
+++ lib/db.ts
@@ -13,21 +13,21 @@ const db = new PrismaClient();
 // }
 
 // test();
-async function test() {
-  const token = await db.sMSToken.create({
-    data: {
-      token: "1212212",
-      user: {
-        connect: {
-          id: 2,
-        },
-      },
-    },
-  });
+// async function test() {
+//   const token = await db.sMSToken.create({
+//     data: {
+//       token: "1212212",
+//       user: {
+//         connect: {
+//           id: 2,
+//         },
+//       },
+//     },
+//   });
 
-  console.log(token);
-}
+//   console.log(token);
+// }
 
-test();
+// test();
 
 export default db;
