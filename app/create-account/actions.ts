b5c5bdeac4363e32fd58b8ad7251864fe57f9847
Sungmin_Kim <dreamerk24dev@gmail.com>
2025-04-30 00:24:49 +0900

    email log in

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -10,6 +10,7 @@ import bcrypt from "bcrypt";
 import { getIronSession } from "iron-session";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
+import getSession from "@/lib/session";
 
 const checkUsername = (username: string) => !username.includes("potato");
 
@@ -105,15 +106,9 @@ export async function createAccount(prevState: any, formData: FormData) {
         id: true,
       },
     });
-    console.log(user);
-    // log the user in
-    const cookie = await getIronSession(await cookies(), {
-      cookieName: "delicious-carrot",
-      password: process.env.COOKIE_PASSWORD!,
-    });
-    //@ts-ignore
-    cookie.id = user.id;
-    await cookie.save();
+    const session = await getSession();
+    session.id = user.id;
+    await session.save();
     // redirect to "/home"
     redirect("/profile");
   }
