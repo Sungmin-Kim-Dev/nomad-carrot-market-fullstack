0899b0bf546b993325825603d4863735f5199128
Sungmin_Kim <dreamerk24dev@gmail.com>
2025-04-30 19:54:10 +0900

    Authentication Middleware

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,33 @@
-import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 import getSession from "./lib/session";
 
+interface Routes {
+  [key: string]: boolean;
+}
+
+// 오브젝트가 어레이보다 값을 찾기가 빠름. 
+// array는 값을 찾으려면 하나씩 다 비교해야 함
+const publicOnlyUrls: Routes = {
+  "/": true,
+  "/login": true,
+  "/sms": true,
+  "/create-account": true,
+};
+
 export async function middleware(request: NextRequest) {
-  const pathname = request.nextUrl.pathname;
-  console.log("Hello!");
+  const session = await getSession();
+  const exist = publicOnlyUrls[request.nextUrl.pathname];
+  if (!session.id) {
+    if (!exist) {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+  } else {
+    if (exist) {
+      return NextResponse.redirect(new URL("/products", request.url));
+    }
+  }
 }
 
 export const config = {
-  matcher: ["/", "/profile", "/create-account", "/user:path*"],
-  
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
 };
